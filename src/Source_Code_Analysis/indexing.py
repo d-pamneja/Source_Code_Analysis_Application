ba436741e1a540b2af7113448ca3f40f16988c69
d-pamneja <dpamneja@gmail.com>
2024-04-22 23:08:20 +0530

    Utils

---
+++ src/Source_Code_Analysis/indexing.py
@@ -0,0 +1,22 @@
+# Importing the dependencies
+from .utils import ingest_repo,load_repo,chunk_split,load_embeddings
+from langchain.vectorstores import Chroma
+
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# PreRequisties
+documents = load_repo("../../repo/")
+text_chunks = chunk_split(documents)
+embeddings = load_embeddings()
+
+# Creating Vector DataBase
+vectordb = Chroma.from_documents(
+    text_chunks,
+    embedding=embeddings,
+    persist_directory= "../../data"
+)
+vectordb.persist()
---
+++ src/Source_Code_Analysis/utils.py
@@ -0,0 +1,64 @@
+# Importing the dependencies
+
+import os
+from git import Repo
+from langchain.text_splitter import Language
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import LanguageParser
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationSummaryMemory
+from langchain.chains import ConversationalRetrievalChain
+
+from dotenv import load_dotenv
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+from src.Source_Code_Analysis.logger import logging
+
+# Repository Ingestion
+def ingest_repo(repo_url):
+    os.makedirs("repo",exist_ok=True)
+    repo_path = "../../repo/"
+    Repo.clone(
+        repo_url,
+        to_path = repo_path
+    )
+    
+# Loading the Repository
+def load_repo(repo_path):
+    loader = GenericLoader.from_filesystem(
+        repo_path,
+        glob="**/*",
+        parser= LanguageParser(language=Language.PYTHON,parser_threshold=500)
+    )
+    
+    documents = loader.load()
+    logging.info("Repositery Loaded")
+    return documents
+
+# Chunking the documents
+def chunk_split(documents):
+    document_splitter = RecursiveCharacterTextSplitter.from_language(
+        language=Language.PYTHON,
+        chunk_size = 2000,
+        chunk_overlap = 200,
+        )
+    
+    text_chunks = document_splitter.split_documents(documents=documents)
+    logging.info(f"Code converted to chunks of size : {len(text_chunks)}")
+    return text_chunks
+
+# Load Embeddings
+def load_embeddings():
+    embeddings = OpenAIEmbeddings(disallowed_special=())
+    return embeddings
+
+
+    
+    
+    
+    
+
+    
